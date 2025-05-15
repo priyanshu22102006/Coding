38c28e9f6448add0eb9e92a154be02543a012084
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2025-05-15 16:24:43 +0530

    launch and task change

--- practice/hello.c
+++ practice/hello.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-    if(ch == 'a' || ch == 'e')
+#include<stdio.h>
+union abc {
+    int a;
+    char b;
+    float c;
+};
+
+void main(){
+    union abc u;
+    u.a = 9;
+    u.b = 97;
+    u.c = 9.85;
+    printf("a = %d\n",u.a);
+    printf("b = %c\n",u.b);
+    printf("c = %f\n",u.c);
+
 }
