e1917d7247bd9502e4dcc470d14ab9e0f3c413ec
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2025-05-22 04:28:28 +0530

    Checked c and c++ file in windows for debuging

--- practice/hello.c
+++ practice/hello.c
@@ -1,17 +1,5 @@
 #include<stdio.h>
-union abc {
-    int a;
-    char b;
-    float c;
-};
-
-void main(){
-    union abc u;
-    u.a = 9;
-    u.b = 97;
-    u.c = 9.85;
-    printf("a = %d\n",u.a);
-    printf("b = %c\n",u.b);
-    printf("c = %f\n",u.c);
-
+int main() {
+    printf("Hello from C!\n");
+    return 0;
 }
