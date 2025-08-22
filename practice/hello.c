f52e553f9d6618edd0ad6e151cb268e901df169d
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2025-08-22 13:55:10 +0530

    Added some liberaries

--- practice/hello.c
+++ practice/hello.c
@@ -1,4 +1,12 @@
-int main(){
-    printf("Hello, World!\n");
-    return 0;
+newNode = (struct node*)malloc(sizeof(struct node)); //malloc() is used to allocate memory and returns a pointer to the allocated memory
+                                                             //size of node is 8 byte so malloc will allocate a node of 8 byte 4 for data and 4 for address
+printf("Enter data: ");
+scanf("%d",&newNode->data);
+newNode->next = NULL;
+if(head == NULL){
+    head = temp = newNode;
+}
+else{
+    temp->next = newNode;
+    temp=newNode;
 }
