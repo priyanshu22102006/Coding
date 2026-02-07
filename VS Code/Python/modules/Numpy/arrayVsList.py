dcccbc438151ee94cc7a83aeb65f9140243767cc
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2026-02-07 22:33:47 +0530

    Added Numpy

---
+++ VS Code/Python/modules/Numpy/arrayVsList.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Creating a NumPy array
+x = np.array([1, 2, 3, 4, 5])
+print(x)
+print(type(x))
+
+# Creating a Python list
+y = [1, 2, 3, 4, 5]
+print(y)
+print(type(y))
+
+# The difference is the array are represented without commas where as lists have commas between elements.
+
+print(np.empty(5))
