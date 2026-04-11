463935606e5595f3aa6256024c90c0af3ca39a2e
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2026-04-11 21:38:23 +0530

    JS did

--- VS Code/JS/first.js
+++ VS Code/JS/first.js
@@ -1,6 +1,9 @@
+console.log("Enternal File :"); 
 let a = 10;
 {
     let b = 20;
-    console.log(a); // Outputs: 20
+    console.log(a); // Outputs: 10
+    console.log(b); // Outputs: 20
 }
-console.log(b); // Outputs: 20
+console.log(a); // Outputs: 10
+console.log(b); // Outputs: ReferenceError: b is not defined
