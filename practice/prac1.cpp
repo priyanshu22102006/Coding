1bd975817b50f4b0e2df1d5f95e13d9b7825e676
Priyanshu Chandra Sarker <priyanshuchandrasarker2210@gmail.com>
2026-09-16 15:26:20 +0530

    made less ususal changes

--- practice/prac1.cpp
+++ practice/prac1.cpp
@@ -1,6 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+
 int main() {
-    cout << "Hello from C++!" <<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N;
+    vector<int> arr(N);
+    for(int i = 0 ; i < N ; i++){
+        cin >> arr[i];
+    }
+    reverse(arr.begin(), arr.end());
+    
+    for (int num : arr) {
+        cout << num << " ";
+    }   
     return 0;
 }
